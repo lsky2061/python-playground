d56ea6f5e6e40dd4b633b18b04d093233fe04e30
lsky2061 <luke.a.corwin@gmail.com>
2024-01-10 20:51:49 -0500

    Edited image with black bar for caption.

--- K_alarm/K_reminder_GUI.py
+++ K_alarm/K_reminder_GUI.py
@@ -5,7 +5,7 @@ import os
 
 font = QFont("Times",32)
 
-image_file_name = 'test_image.png'
+image_file_name = 'default_image.jpg'
 #Below is the command that should give us the path to the directory that
 #contains this file, regardless of where we run it from
 working_dir = f'{os.getcwd()}/{os.path.dirname(__file__)}'
